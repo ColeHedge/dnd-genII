497e02631f4061a41e261064d0a37c2f653e90b2
DESKTOP-PFKP3C7\Coleman <colemanhedge94@gmail.com>
2017-12-04 15:30:08 -0800

    Pulled Name value from database

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,20 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
+admin.initializeApp(functions.config().firebase);
 
 exports.helloWorld = functions.https.onRequest((req, res) => {
+	var db = admin.database();
  	res.set('Access-Control-Allow-Origin', "*")
   	res.set('Access-Control-Allow-Methods', 'GET, POST')
   	var data = req.query.data;
-  	res.status(200).send(data);
+  	var ref = db.ref('/0').once('value')
+  		.then(function(snapshot) {
+  			var name = snapshot.child("name").val();
+  			res.status(200).send("database is connected Maybe? " + name);
+  		});
 });
